3409693976ef7a053dc854fac71c5159d8abe5ed
Xenia <lommy101@gmail.com>
2020-02-27 20:41:28 +0900

    fixed something..

--- app.py
+++ app.py
@@ -2,20 +2,26 @@ from flask import Flask, render_template
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import requests
+import json
+
+import urllib.request
+import urllib.parse
+
+headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0'}
 
 app = Flask(__name__)
-
+app.debug = True
 @app.route("/")
 def home():
-    data = api()
-    return render_template('index.html', data=data)
+    print(GetLocCount())
+    return render_template('index.html', data=GetTotalCount())
 
-def api():
+def GetTotalCount():
     URL = "http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun="
-    html = urlopen(URL)
-    res = requests.get(URL)
+    res = requests.get(URL,headers=headers)
     soup = BeautifulSoup(res.content, 'html.parser')
-    data = soup.select('.s_listin_dot > li')[0:4]
+    data = soup.select('.w_bold')[0:4]
+    #data = soup.select('.s_listin_dot > li')[0:4]
     result = list()
     for x in data:
         result.append(x.get_text().replace(',','').split(' ')[-1].replace('명', ''))
@@ -24,8 +30,29 @@ def api():
         {'name' : '사망자'   , 'tag' : 'dark'    , 'count' : result[2]},
         {'name' : '검사진행' , 'tag' : 'warning' , 'count' : result[3]},
         {'name' : '격리해제' , 'tag' : 'primary' , 'count' : result[1]}]
-
     return data
 
+def GetLocCount():
+    URL = "http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=13&ncvContSeq=&contSeq=&board_id=&gubun="
+    res = requests.get(URL,headers=headers)
+    soup = BeautifulSoup(res.content, 'html.parser')
+    table = soup.find('table', attrs={'class':'num'})
+    table_body = table.find('tbody')
+
+    location = list()
+    count = list()
+    rows = table_body.find_all('tr')
+    for row in rows:
+        loc = row.find_all('th')
+        loc = [ele.text.strip() for ele in loc]
+        location.append(loc[0])
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        count.append(int(cols[0].replace(',','')))
+    location = location[1:-1]
+    count    = count[1:-1]
+    data = dict(zip(location, count))
+
+    print(data)
 if __name__ == "__main__":
-    app.run(host="localhost", port=8080)
+    app.run(host="localhost", port=8080, use_reloader=False)
